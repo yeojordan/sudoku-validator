b30d173f00e3baf0f4545d6f4f48db54f03ce78f
Jordan Yeo <jordanyeo23@gmail.com>
2017-04-16 15:07:07 +0800

    Semaphores Test

--- partA/mssv.c
+++ partA/mssv.c
@@ -28,19 +28,26 @@ int main (int argc, char* argv[])
     int readStatus;
     int numbers[] = {0,0,0,0,0,0,0,0,0};
     int row, i, j;
+    Region* region;
+    sem_t semFull, semEmpty, semMutex, *semaphores;
+    int processNum = 0;
+    int pid;
 
     // File Descriptors
-    int buff1FD, buff2FD, counterFD;
+    int buff1FD, buff2FD, counterFD, semFD, regionFD;
 
     // Shared memory sizes
     size_t buff1Sz = sizeof(int) * 11;
     size_t buff2Sz = sizeof(int) * 9 * 9;
     size_t countSz = sizeof(int) * 1;
+    size_t semSz = sizeof(sem_t);
+    size_t regionSz = sizeof(Region);
 
     // Shared memory pointers
-    int* buff1Ptr;
-    int (*buff2Ptr)[NINE][NINE];
+    int* buff2Ptr;
+    int (*buff1Ptr)[NINE][NINE];
     int* countPtr;
+
 
     // Matrix
     int truncStat1 = 0;
@@ -51,8 +58,12 @@ int main (int argc, char* argv[])
     buff1FD = shm_open("buffer1", O_CREAT | O_RDWR, 0666);
     buff2FD = shm_open("buffer2", O_CREAT | O_RDWR, 0666);
     counterFD = shm_open("counter", O_CREAT | O_RDWR, 0666);
+    semFD = shm_open("semaphores", O_CREAT | O_RDWR, 0666);
+    regionFD = shm_open("region", O_CREAT | O_RDWR, 0666);
 
-    if ( buff1FD == -1 || buff2FD == -1 || counterFD == -1 )
+
+    if ( buff1FD == -1 || buff2FD == -1 || counterFD == -1 || semFD == -1 ||
+            regionFD == -1 )
     {
         fprintf( stderr, "Error creating shared memory blocks\n" );
         return -1;
@@ -62,6 +73,8 @@ int main (int argc, char* argv[])
     truncStat1 = ftruncate(buff1FD, buff1Sz);
     truncStat2 = ftruncate(buff2FD, buff2Sz);
     truncStat3 = ftruncate(counterFD, countSz);
+    ftruncate( semFD, sizeof(sem_t) * 3 );
+    ftruncate( regionFD, sizeof(Region));
 
     printf("%d %d %d\n", truncStat1, truncStat2, truncStat3);
     // if (truncStat1 == -1 || truncStat2 == -1 || truncStat3 == -1)
@@ -71,29 +84,128 @@ int main (int argc, char* argv[])
     // }
 
     // Memory mapping
-    buff1Ptr = (int*) mmap(NULL, buff1Sz, PROT_READ | PROT_WRITE, MAP_SHARED, buff1FD, 0);
-    buff2Ptr = mmap(NULL, buff2Sz, PROT_READ | PROT_WRITE, MAP_SHARED, buff2FD, 0);
+    buff2Ptr = (int*) mmap(NULL, buff2Sz, PROT_READ | PROT_WRITE, MAP_SHARED, buff2FD, 0);
+    buff1Ptr = mmap(NULL, buff1Sz, PROT_READ | PROT_WRITE, MAP_SHARED, buff1FD, 0);
     countPtr = (int*) mmap(NULL, countSz, PROT_READ | PROT_WRITE, MAP_SHARED, counterFD, 0);
+    semaphores = mmap(NULL, sizeof(sem_t) * 3, PROT_READ | PROT_WRITE, MAP_SHARED, semFD, 0);
+    region = mmap(NULL, sizeof(Region), PROT_READ | PROT_WRITE, MAP_SHARED, regionFD, 0);
+
+
+    if ((sem_init(&semMutex, 1, 1)== 1)
+        || (sem_init(&semFull, 1, 0) == 1)
+            ||(sem_init(&semEmpty, 1, 1) == 1))
+    {
+        fprintf(stderr, "Could not initialise semaphores\n");
+        exit(1);
+    }
+
+    semaphores[0] = semMutex;
+    semaphores[1] = semFull;
+    semaphores[2] = semEmpty;
+
 
     // Initialise counter
     *countPtr = 0;
-
+row = 0;
     // Read input file
-    readStatus = readFile(inputFile, NINE, NINE, buff2Ptr);
-
-
+    readStatus = readFile(inputFile, NINE, NINE, buff1Ptr);
     if (readStatus != 0)
     {
         printf("Error reading contents of file");
         return -1;
     }
 
+    pid = -1;
+    processNum = 0;
 
+    // Create child processes for rows
+    while( processNum < NINE && pid != 0 )
+    {
+        printf("MAKING CHILDREN %d!\n", processNum);
+
+        signal(SIGCHLD, SIG_IGN);
+        pid = fork();
+        processNum++;
+
+
+    }
+
+    // Child
+    if( pid == 0)
+    {
+        // Check rows
+        for (i = 0; i < NINE; i++)
+        {
+            printf("Checking row: %d\n", i+1);
+            //row = checkRow( numbers, i, 9, buff1Ptr );
+
+            numbers[((*buff1Ptr)[processNum][i])-1]++;
+
+        }
+
+
+        //printf("%d\n", *countPtr);
+printf("Waiting for process %d\n", processNum );
+                        //acquire locks
+                        sem_wait(&(semaphores[2]));//Empty lock
+                        sem_wait(&(semaphores[0]));//Mutex lock
+                        //put into subtotal along with PID
+                        //
+printf("Finished waiting for %d\n", processNum );
+
+
+
+                        region->type = ROW;
+                        region->positionX = processNum;
+                        region->positionY = 0;
+                        region->pid = getpid();
+                        region->valid = checkValid(numbers);
+                    printf("Process ID: %d - Row: %d - Valid: %d\n", region->pid, processNum, region->valid);
+                        //release locks
+                        sem_post(&(semaphores[0]));
+                        sem_post(&(semaphores[1]));
+
+                        //resetArray(numbers);
+    }
+    // Parent
+    else if ( pid > 0)
+    {
+
+        for(int ii = 0; ii < NINE; ii++)
+        {
+printf("HI\n");
+
+            //wait
+            sem_wait(&(semaphores[1]));//Lock full
+            sem_wait(&(semaphores[0]));//Lock mutex
+
+            printf("Process ID-%d: row %d is valid\n",
+                    region->pid, (region->positionX)+1 );
+
+
+            sem_post(&(semaphores[0]));//Unlock mutex
+            sem_post(&(semaphores[2]));//Unlock empty
+        }
+    }
+
+    // Create child for column
+    // if( pid == 0)
+    // {
+    //
+    // }
+
+    // Create child for sub regions
+    // if(pid == 0)
+    // {
+    //
+    // }
+
+/*
     // Check rows
     for (i = 0; i < 9; i++)
     {
         printf("Checking row: %d\n", i+1);
-        row = checkRow( numbers, i, 9, buff2Ptr );
+        row = checkRow( numbers, i, 9, buff1Ptr );
         if ( row != 0)
         {
             // Write to log file
@@ -113,7 +225,7 @@ int main (int argc, char* argv[])
     for (i = 0; i < 9; i++)
     {
         printf("Checking column: %d\n", i+1);
-        row = checkCol(numbers, 9, i+1, buff2Ptr);
+        row = checkCol(numbers, 9, i+1, buff1Ptr);
         if ( row != 0)
         {
             // Write to log file
@@ -137,7 +249,7 @@ int main (int argc, char* argv[])
         for (j = 0; j < SUB; j++)
         {
             printf("Checking subgrid %d, %d\n", i*SUB, j*SUB);
-            row = checkSub(numbers, i*SUB, j*SUB, buff2Ptr);
+            row = checkSub(numbers, i*SUB, j*SUB, buff1Ptr);
 
             if ( row != 0)
             {
@@ -156,7 +268,7 @@ int main (int argc, char* argv[])
     }
 
     printf("%d\n", *countPtr);
-    
+*/
     // Clean up shared memory
     munmap(buff1Ptr, buff1Sz);
     munmap(buff2Ptr, buff2Sz);
@@ -280,4 +392,21 @@ void resetArray(int numbers[])
     {
         numbers[i] = 0;
     }
+}
+
+int checkValid(int numbers[])
+{
+    int j;
+    for ( j = 0; j < 9; j++ )
+    {
+        if ( numbers[j] != 1)
+        {
+            //printf("Invalid Row: %d\n", rows+1);
+            return (FALSE);
+            //status = rows + 1;
+        }
+        //numbers[j] = 0;
+    }
+
+    return TRUE;
 }
--- partA/mssv.h
+++ partA/mssv.h
@@ -1,17 +1,39 @@
 
 
+#include "fileIO.h"
+
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <math.h>
 #include <fcntl.h>
 #include <semaphore.h>
 #include <sys/mman.h>
+#include <unistd.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
-#include "fileIO.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+
+
 
 #define NINE 9
 #define SUB 3
+#define NUMPROCESSES 11
+#define FALSE 0
+#define TRUE !FALSE
+
+typedef enum {ROW, COL, SUB_REGION} Region_Type;
+
+typedef struct
+{
+    Region_Type type;
+    int positionX;
+    int positionY;
+    pid_t pid;
+    int valid;
+} Region;
+
 
 int checkRow(int numbers[], int rows, int cols, int (*matrix)[NINE][NINE] );
 int checkCol(int numbers[], int rows, int cols, int (*matrix)[NINE][NINE] );

8b94d018f0e9c87f5c41c238704b5999b5d03527
Your Name <you@example.com>
2017-04-18 10:21:24 +0800

    PARENT BLOCKED YAY

--- partA/mssv.c
+++ partA/mssv.c
@@ -31,12 +31,12 @@ int main (int argc, char* argv[])
 
     int row, i, j;
     Region* region;
-    sem_t semFull, semEmpty, semMutex, *semaphores;
+    sem_t semFull, semEmpty, semMutex, semParent, *semaphores;
     int processNum = 0;
     int pid;
     int numValid;
     char format[500];
-
+    int resFD;
     // File Descriptors
     int buff1FD, buff2FD, counterFD, semFD, regionFD;
 
@@ -51,7 +51,7 @@ int main (int argc, char* argv[])
     int* buff2Ptr;
     int (*buff1Ptr)[NINE][NINE];
     int* countPtr;
-
+    int* resourceCount;
 
     // Matrix
     int truncStat1 = 0;
@@ -64,6 +64,7 @@ int main (int argc, char* argv[])
     counterFD = shm_open("counter", O_CREAT | O_RDWR, 0666);
     semFD = shm_open("semaphores", O_CREAT | O_RDWR, 0666);
     regionFD = shm_open("region", O_CREAT | O_RDWR, 0666);
+    resFD = shm_open("resources", O_CREAT | O_RDWR, 0666);
 
 
     if ( buff1FD == -1 || buff2FD == -1 || counterFD == -1 || semFD == -1 ||
@@ -77,8 +78,9 @@ int main (int argc, char* argv[])
     truncStat1 = ftruncate(buff1FD, buff1Sz);
     truncStat2 = ftruncate(buff2FD, buff2Sz);
     truncStat3 = ftruncate(counterFD, countSz);
-    ftruncate( semFD, sizeof(sem_t) * 3 );
-    ftruncate( regionFD, sizeof(Region));
+    ftruncate( semFD, sizeof(sem_t) * 4 );
+    ftruncate( regionFD, sizeof(Region)*11);
+    ftruncate( resFD, sizeof(int));
 
     printf("%d %d %d\n", truncStat1, truncStat2, truncStat3);
     // if (truncStat1 == -1 || truncStat2 == -1 || truncStat3 == -1)
@@ -91,13 +93,14 @@ int main (int argc, char* argv[])
     buff2Ptr = (int*) mmap(NULL, buff2Sz, PROT_READ | PROT_WRITE, MAP_SHARED, buff2FD, 0);
     buff1Ptr = mmap(NULL, buff1Sz, PROT_READ | PROT_WRITE, MAP_SHARED, buff1FD, 0);
     countPtr = (int*) mmap(NULL, countSz, PROT_READ | PROT_WRITE, MAP_SHARED, counterFD, 0);
-    semaphores = mmap(NULL, sizeof(sem_t) * 3, PROT_READ | PROT_WRITE, MAP_SHARED, semFD, 0);
-    region = mmap(NULL, sizeof(Region), PROT_READ | PROT_WRITE, MAP_SHARED, regionFD, 0);
+    semaphores = mmap(NULL, sizeof(sem_t) * 4, PROT_READ | PROT_WRITE, MAP_SHARED, semFD, 0);
+    region = mmap(NULL, sizeof(Region)* 11, PROT_READ | PROT_WRITE, MAP_SHARED, regionFD, 0);
 
-
+    resourceCount = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, resFD, 0);
     if ((sem_init(&semMutex, 1, 1)== 1)
         || (sem_init(&semFull, 1, 0) == 1)
-            ||(sem_init(&semEmpty, 1, 1) == 1))
+            ||(sem_init(&semEmpty, 1, 1) == 1)
+                ||(sem_init(&semParent, 1, 1) == 1))
     {
         fprintf(stderr, "Could not initialise semaphores\n");
         exit(1);
@@ -106,6 +109,7 @@ int main (int argc, char* argv[])
     semaphores[0] = semMutex;
     semaphores[1] = semFull;
     semaphores[2] = semEmpty;
+    semaphores[3] = semParent;
 
 
     // Initialise counter
@@ -122,7 +126,9 @@ row = 0;
 
     pid = -1;
     processNum = 0;
+    *resourceCount = 0;
 
+            sem_wait(&(semaphores[3])); 
     // Create child processes for rows
     while( processNum < 11 && pid != 0 )
     {
@@ -130,11 +136,14 @@ row = 0;
         signal(SIGCHLD, SIG_IGN);
         pid = fork();
     
+    // Parent aquires lock of resourceCount
         // Store child's pid in array
         if ( pid > 0)
         {
             printf("Child ID: %d, processNum: %d\n", pid, processNum);
-            childIDs[processNum] = pid;    
+            childIDs[processNum] = pid;
+            *resourceCount = *resourceCount + 1;
+            printf("Parent's resourceCount: %d\n", *resourceCount);           
         }
         processNum++;
 
@@ -159,18 +168,18 @@ row = 0;
 
         //printf("%d\n", *countPtr);
                         //acquire locks
-                        sem_wait(&(semaphores[2]));//Empty lock
+            //            sem_wait(&(semaphores[2]));//Empty lock
                         sem_wait(&(semaphores[0]));//Mutex lock
                         
 
-                        region->type = ROW;
-                        region->positionX = processNum;
-                        region->positionY = 0;
-                        region->pid = getpid();
-                        region->valid = checkValid(numbers);
+                        region[processNum-1].type = ROW;
+                        region[processNum-1].positionX = processNum;
+                        
+                        region[processNum-1].pid = getpid();
+                        region[processNum-1].valid = checkValid(numbers);
                         // Update buffer2
                         numValid = 0;
-                        if (region->valid == TRUE)
+                        if (region[processNum-1].valid == TRUE)
                         {  
                             numValid = 1;  
                         }
@@ -178,7 +187,7 @@ row = 0;
                         {
                             sprintf(format, "row %d is invalid\n", processNum);
 printf("    %s", format);
-                            writeFile(region, format);
+                            writeFile(&(region[processNum-1]), format);
                             
                         }
                        
@@ -188,7 +197,7 @@ printf("    %s", format);
 
                         sem_post(&(semaphores[0]));
                         // Update counter
-                        sem_post(&(semaphores[1]));
+            //            sem_post(&(semaphores[1]));
 			//kill(getpid(), SIGTERM);
                         //resetArray(numbers);
         }
@@ -236,19 +245,19 @@ printf("    %s", format);
 	        }
 
 	            sprintf(format + strlen(format), "are invalid\n"); 
-			            sem_wait(&(semaphores[2]));//Empty lock
+			       //     sem_wait(&(semaphores[2]));//Empty lock
                         sem_wait(&(semaphores[0]));//Mutex lock
 	   		
-                        region->type = COL;
-                        region->positionX = validCol;
-                        region->pid = getpid();
+                        region[processNum-1].type = COL;
+                        region[processNum-1].positionX = validCol;
+                        region[processNum-1].pid = getpid();
 if(validCol != 9)
 {
     printf("    %s", format);
-    writeFile(region, format);    
+    writeFile(&(region[processNum-1]), format);    
 }
                         // Update buffer2
-                        numValid = region->positionX;
+                        numValid = region[processNum-1].positionX;
                        
                         buff2Ptr[processNum-1] = validCol;
 
@@ -256,7 +265,7 @@ if(validCol != 9)
                         *countPtr = *countPtr + validCol;
 		                
                         sem_post(&(semaphores[0]));
-                        sem_post(&(semaphores[1]));
+                    //    sem_post(&(semaphores[1]));
 //		kill(getpid(), SIGTERM);
         }
 
@@ -305,19 +314,19 @@ if(validCol != 9)
             }
     
 		    sprintf(format+strlen(format), "is invalid\n");
-			            sem_wait(&(semaphores[2]));//Empty lock
+			        //    sem_wait(&(semaphores[2]));//Empty lock
                         sem_wait(&(semaphores[0]));//Mutex lock
                         //put into subtotal along with PID
                         //
 
-                        region->type = SUB_REGION;
-                        region->positionX = validSub;
-                        region->pid = getpid();
+                        region[processNum-1].type = SUB_REGION;
+                        region[processNum-1].positionX = validSub;
+                        region[processNum-1].pid = getpid();
                         // Update buffer2
                         
 if(validSub != 9)
 {
-    writeFile(region, format);    
+    writeFile(&(region[processNum-1]), format);    
 }
                        
                         buff2Ptr[processNum-1] = validSub;
@@ -327,10 +336,19 @@ if(validSub != 9)
                         //release locks
 
                         sem_post(&(semaphores[0]));	
-                        sem_post(&(semaphores[1]));
+                    //    sem_post(&(semaphores[1]));
 
 		//	kill(getpid(), SIGTERM);
         }
+
+
+        // Child signals it is finished by incremented resourceCount
+        sem_wait(&(semaphores[3]));
+        sem_wait(&(semaphores[0]));
+            printf("I'm done! pid-%d resCount = %d\n", getpid(), (*resourceCount)-1);
+            *resourceCount = *resourceCount - 1;
+        sem_post(&(semaphores[0]));
+        sem_post(&(semaphores[3]));
     }
 
 
@@ -339,13 +357,29 @@ if(validSub != 9)
 
     if ( pid > 0)
     {
+        sem_post(&(semaphores[3]));
 	int count=0;
-	  //  for(int jj = 0; jj < 11; jj++)
-      //  {
-       //     waitpid(childIDs[jj], NULL, 0);
-            printf("Parent Waiting for Children\n");    
-      //  }
+    int done = FALSE;
+	    while( !done )
+        {
+printf("Parent Waiting for Children\n");    
 
+            sem_wait(&(semaphores[3]));
+        sem_wait(&(semaphores[0]));
+            if ( *resourceCount == 0)
+            {
+                done = TRUE;
+                printf("DONE\n");
+            }
+        sem_post(&(semaphores[0]));
+            sem_post(&(semaphores[3]));
+
+        }
+
+       
+       //     sem_wait(&(semaphores[1]));//Lock full
+       //     sem_wait(&(semaphores[0]));//Lock mutex
+       
         for(int ii = 0; ii < 11; ii++)
         {
             
@@ -353,49 +387,51 @@ if(validSub != 9)
 	    int position;
 
             //wait
-            sem_wait(&(semaphores[1]));//Lock full
-            sem_wait(&(semaphores[0]));//Lock mutex
 
-            if (region->type == ROW)
+        //    sem_wait(&(semaphores[1]));//Lock full
+            sem_wait(&(semaphores[0]));//Lock mutex
+            if (region[ii].type == ROW)
             {
                 type = "row";
-		position = region->positionX;
+		position = region[ii].positionX;
 
-		validRegion(&count, region);
-		if ( region->valid == TRUE)
+		//validRegion(&count, region);
+		if ( region[ii].valid == TRUE)
 		{
 			printf("Validation result from process ID-%d: %s %d is valid\n",
-                    		region->pid,type, position);
+                    		region[ii].pid,type, position);
             	}
 		else
 		{
 			printf("Validation result from process ID-%d: %s %d is invalid\n",
-				region->pid, type, position);
+				region[ii].pid, type, position);
 		}
 
 	    }
-            else if (region->type == COL)
+            else if (region[ii].type == COL)
             {
                 type = "column";
-		validRegion(&count, region);
-		position = region->positionX;
+		//validRegion(&count, region);
+		position = region[ii].positionX;
 
-		printf("Validation result from process ID-%d: %d out of 9 columns are valid\n",region->pid, region->positionX);
+		printf("Validation result from process ID-%d: %d out of 9 columns are valid\n",region[ii].pid, region[ii].positionX);
             }
             else
             {
                 type = "sub-grid";
-		validRegion(&count, region);
-		position = region->positionX;
+		//validRegion(&count, region);
+		position = region[ii].positionX;
 
-		printf("Validation result from process ID-%d: %d out of 9 sub-grids are valid\n",region->pid, region->positionX);
+		printf("Validation result from process ID-%d: %d out of 9 sub-grids are valid\n",region[ii].pid, region[ii].positionX);
 		
             }
 
             sem_post(&(semaphores[0]));//Unlock mutex
-            sem_post(&(semaphores[2]));//Unlock empty
+           // sem_post(&(semaphores[2]));//Unlock empty
         }
 
+     //       sem_post(&(semaphores[0]));//Unlock mutex
+     //       sem_post(&(semaphores[2]));//Unlock empty
 	char* message;
 
 	if (*countPtr == 27)
@@ -415,14 +451,16 @@ printf("Children are finished\n");
     munmap(buff1Ptr, buff1Sz);
     munmap(buff2Ptr, buff2Sz);
     munmap(countPtr, countSz);
-    munmap(semaphores, sizeof(sem_t)*3);
-    munmap(region, sizeof(Region));
- 
+    munmap(semaphores, sizeof(sem_t)*4);
+    munmap(region, sizeof(Region)*11);
+    munmap(resourceCount, sizeof(int));
+
     shm_unlink("buffer1");
     shm_unlink("buffer2");
     shm_unlink("counter");
     shm_unlink("semaphores");
     shm_unlink("region");
+    shm_unlink("resources");
 }
 
 
